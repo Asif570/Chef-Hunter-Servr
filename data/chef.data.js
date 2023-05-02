48da75606fb5c2066574d766e760c3e60a5b2fbe
Asif570 <islamasif570@gmail.com>
2023-05-02 22:21:59 +0600

    Create Data Done

---
+++ data/chef.data.js
@@ -0,0 +1,74 @@
+const AllChef = [
+  {
+    id: "1",
+    name: " Martin Yan",
+    photo_url:
+      "https://www.discoverwalks.com/blog/wp-content/uploads/2022/05/martinyan-wikipedia.jpg",
+    speacial_foods_id: [9, 13, 7, 12],
+  },
+  {
+    id: "2",
+    name: "Cecilia Chiang",
+    photo_url:
+      "https://www.discoverwalks.com/blog/wp-content/uploads/2022/05/cecilia-chiang-the-new-york-times-1365x2048.jpg",
+    speacial_foods_id: [8, 1, 3, 5],
+  },
+  {
+    id: "3",
+    name: "Peter Chang",
+    photo_url:
+      "https://www.discoverwalks.com/blog/wp-content/uploads/2022/05/peter-chang-the-new-york-times-1536x1024.jpg",
+    speacial_foods_id: [1, 4, 7, 5, 3],
+  },
+  {
+    id: "4",
+    name: "Shirley Chung",
+    photo_url:
+      "https://www.discoverwalks.com/blog/wp-content/uploads/2022/05/shirley-chung-bravo-tv-1024x1024.jpg",
+    speacial_foods_id: [1, 2, 3, 7],
+  },
+  {
+    id: "5",
+    name: "Susanna Foo",
+    photo_url:
+      "https://www.discoverwalks.com/blog/wp-content/uploads/2022/05/susanna-foo-the-spruce-eats-1024x1024.jpg",
+    speacial_foods_id: [7, 5, 3, 6, 9],
+  },
+  {
+    id: "6",
+    name: "Chan Yan-tak",
+    photo_url:
+      "https://www.discoverwalks.com/blog/wp-content/uploads/2022/05/chan-yan-tak-alchetron-683x1024.jpeg",
+    speacial_foods_id: [1, 6, 7, 3],
+  },
+  {
+    id: "7",
+    name: "Mei Lin",
+    photo_url:
+      "https://www.discoverwalks.com/blog/wp-content/uploads/2022/05/mei-lin-bravo-tv-1536x1024.jpg",
+    speacial_foods_id: [8, 5, 2, 4],
+  },
+  {
+    id: "8",
+    name: "Tang Chi Keung",
+    photo_url:
+      "https://chineserestaurantawards.com/wp-content/uploads/2022/10/Chef-Tang-Chi-Keung-Peninsula-Hotels.jpg?x15138",
+    speacial_foods_id: [3, 1, 5, 8],
+  },
+  {
+    id: "9",
+    name: "Manpreet Sethi",
+    photo_url:
+      "https://chineserestaurantawards.com/wp-content/uploads/2022/10/Chef-Manpreet-Sethi-Paradox-Hotel-Vancouver.jpg?x15138",
+    speacial_foods_id: [2, 6, 4, 12],
+  },
+  {
+    id: "10",
+    name: "Alex Chen",
+    photo_url:
+      "https://chineserestaurantawards.com/wp-content/uploads/2022/10/Chef-Alex-Chen-Boulevard-and-Wild-Blue.jpg?x15138",
+    speacial_foods_id: [6, 9, 4, 11],
+  },
+];
+
+export default AllChef;
